1d9633ac8a066346a354511627762f45f2ab09a4
Shivasaiyadav <160626636+Shivasaiyadav08@users.noreply.github.com>
2025-09-21 12:02:56 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { useState } from "react";
 import Login from "./pages/Login";
-import Register from "./pages/register";
+import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
 import ForgotPassword from "./pages/Forgotpassword";
 import ResetPassword from "./pages/ResetPassword"; // ✅ import this
